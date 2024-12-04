c4b538a413168c5e593d677966f6c45d7efe3a5a
wimpli <sophie.wimplinger@htl1.at>
2024-12-04 15:41:33 +0100

    Erstellung Szene 1 Basics, Aufbau der Szenen

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-import { gsap } from "gsap";
-    
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-
 gsap.registerPlugin(ScrollTrigger);
+
+// Szene 1 ------------------------------------------------------------
+
+gsap.set('.ganzerBrief', { x: -750 })
+gsap.set('.ganzerBrief', { y: -100 })
+gsap.set('.letter', { y: 500 })
+
+const tlBrief = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.scene1',
+        markers: true,
+        scrub: 2,
+        pin: true,
+    }
+})
+
+tlBrief.to('.ganzerBrief', { x: 475 })
+tlBrief.to('.letter', { y: -950, })
+
+// ------------------------------------------------------------------------
