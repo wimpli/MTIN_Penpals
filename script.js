4e67804cd81c5c965fa17baef0b905876ce73df9
lorischnee09 <lorina.schneeberger@htl1.at>
2024-12-05 16:51:48 +0100

    Szene5

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ tlBrief.to('.ganzerBrief', { x: 475, duration: 10});
 tlBrief.to('.letter', { y: -950, duration:15, end: '+=00000'});
 
 // ------------------------------------------------------------------------
+
+// Szene 5 ------------------------------------------------------------
+
+gsap.to(".middle", {
+    scrollTrigger: {
+        trigger: ".middle",
+        start: "top center",    
+        end: "bottom+=200 center",
+        scrub: true,
+        markers: true,
+    },
+    y: 300,
+});
